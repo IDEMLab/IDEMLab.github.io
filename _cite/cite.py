73a5c9dc9d1eb4b850287dc056bd902764ac96ad
IDEMLab <michelmannlab@nyu.edu>
2025-04-25 13:31:21 -0400

    Update cite.py

--- _cite/cite.py
+++ _cite/cite.py
@@ -28,7 +28,7 @@ log("Compiling sources")
 sources = []
 
 # in-order list of plugins to run
-plugins = ["google-scholar", "pubmed", "orcid", "sources"]
+plugins = ["google-scholar"]
 # plugins = ["pubmed", "orcid", "sources"]
 
 # loop through plugins
